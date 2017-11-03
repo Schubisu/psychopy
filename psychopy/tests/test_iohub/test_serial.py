251f3e890c3fc91c5db490f8f7b9cfe7e7f190f6
schubisu <robin.schubert@ghi-muenster.de>
2017-11-03 17:35:02 +0100

    test: added tests for iohub.serial_device

--- psychopy/tests/test_iohub/test_serial.py
+++ psychopy/tests/test_iohub/test_serial.py
@@ -1,46 +1,73 @@
 from __future__ import print_function
+
+from builtins import object
+import pytest
 import time
-from psychopy import core, visual
 from psychopy.iohub import launchHubServer
 
-# Settings for serial port communication.
-SERIAL_PORT = '/dev/null'
-BAUDRATE = 38400
 
-event_parser_info = dict(parser_function="psychopy.iohub.devices.serial_dummy.custom_dummy_parser")
-# configure iohub
-exp_code = 'serial_demo'
-sess_code = 'S_{0}'.format(int(time.mktime(time.localtime())))
-iohubkwargs = {'experiment_code': exp_code,
-               'session_code': sess_code,
-               'serial_dummy.SerialDummy': dict(
-                   name='serial',
-                   port=SERIAL_PORT,
-                   baud=BAUDRATE,
-                   parity='NONE',
-                   bytesize=8,
-                   event_parser=event_parser_info)}
+class TestSerial(object):
+    @classmethod
+    def setup(cls):
+        event_parser_info = {
+            'parser_function': "psychopy.iohub.devices.serial_dummy.custom_dummy_parser"
+        }
+        cls.iohubkwargs = {
+            'experiment_code': 'serial_dummy_test',
+            'session_code': 'serial_dummy_test',
+            'serial_dummy.SerialDummy': dict(
+                name='serial_dummy',
+                port='/dev/null',
+                baud=38400,
+                parity='NONE',
+                bytesize=8,
+                event_parser=event_parser_info)}
 
-io = launchHubServer(**iohubkwargs)
-keyboard = io.devices.keyboard
-serial_device = io.devices.serial
-serial_device.enableEventReporting(True)
+    def test_launch_quit_hub_server(self):
+        import psychopy
+        io = launchHubServer(**self.iohubkwargs)
+        assert isinstance(io, psychopy.iohub.client.ioHubConnection)
+        io.quit()
 
-io.clearEvents('all')
+    def test_serial_device(self):
+        io = launchHubServer(**self.iohubkwargs)
+        serial_device = io.devices.serial_dummy
+        assert serial_device.getName() == 'serial_dummy', 'incorrect device name'
+        assert serial_device.isConnected() is True, 'device is not connected'
+        assert serial_device.isReportingEvents() is False, 'device reporting events'
+        serial_device.close()
+        io.quit()
 
-# Check for keyboard and serial events.
-# Exit on keyboard press event.
-# Print any serial events.
-#
-while not keyboard.getPresses():
-    serial_device.write("TEST")
-    for serevt in serial_device.getEvents():
-        print(serevt)
-    io.wait(.500)
+    def test_event_reporting(self):
+        io = launchHubServer(**self.iohubkwargs)
+        serial_device = io.devices.serial_dummy
+        serial_device.enableEventReporting(True)
+        io.clearEvents('all')
+        assert serial_device.isReportingEvents() is True, 'device not reporting events'
 
-# Stop recording events from the PST box and switch off all lamps.
-serial_device.close()
-serial_device.enableEventReporting(False)
+        serial_device.write("TEST")
+        time.sleep(.1)
+        events = serial_device.getEvents()
+        assert len(events) == 1, 'incorrect number of events'
 
-# Close the window and quit the program.
-io.quit()
+        event = events[0]
+        assert event.data == "TEST", 'incorrect response'
+        assert event.port == '/dev/null', 'incorrect port'
+        assert event.time - event.device_time < 0.001, 'time mismatch'
+        assert event.time - event.logged_time < 0.001, 'time mismatch'
+        #assert event.time < 0.1, 'time mismatch'
+        serial_device.close()
+        io.quit()
+
+    def test_close_serial(self):
+        io = launchHubServer(**self.iohubkwargs)
+        serial_device = io.devices.serial_dummy
+        serial_device.close()
+        serial_device.enableEventReporting(False)
+        assert serial_device.isConnected() is False
+        assert serial_device.isReportingEvents() is False
+        io.quit()
+
+
+if __name__ == "__main__":
+    pytest.main()
